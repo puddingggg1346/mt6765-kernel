9fe4170c92a6116273aaaa18ba6e0677cf44a9a9
puddingggg1346 <puddingggg1346@users.noreply.github.com>
2026-08-08 13:23:25 +0800

    use extern inline decl for uclamp_se_set

--- build-kernel.sh
+++ build-kernel.sh
@@ -4,14 +4,12 @@ export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 git clone --depth 1 --branch 4.19.191_mt6765 https://github.com/KrutosVIP/generic_kernel_mediatek_alps.git
 cd generic_kernel_mediatek_alps
-sed -i '2365,2366c\
-static inline void uclamp_se_set(struct uclamp_se *uc_se,\
-                                 unsigned int value, bool user_defined)\
-{\
-        uc_se->value = value;\
-        uc_se->user_defined = user_defined;\
-}' kernel/sched/sched.h
-sed -n '2363,2372p' kernel/sched/sched.h
+
+# 修复: sched.h 中 uclamp_se_set 用 extern inline 声明（core.c已有external inline定义）
+sed -i '2365c\
+extern inline void uclamp_se_set(struct uclamp_se *uc_se,\
+                                 unsigned int value, bool user_defined);' kernel/sched/sched.h
+
 make ARCH=arm64 k65v1_64_bsp_defconfig
 scripts/config --enable SYSVIPC
 scripts/config --enable IPC_NS
